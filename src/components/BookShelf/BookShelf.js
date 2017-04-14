58871a87faa263de194ad9b7a61e76b58b2f8787
liumin1128 <970568830@qq.com>
2017-04-14 18:47:16 +0800

    增加首页悬浮按钮

--- src/components/BookShelf/BookShelf.js
+++ src/components/BookShelf/BookShelf.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { routerRedux } from 'dva/router';
 import styles from './BookShelf.less';
 
-function BookShelf({ list }) {
+function BookShelf({ dispatch, list }) {
+  function gotoUrl(id) {
+    dispatch(routerRedux.push({
+      pathname: '/reader',
+      query: { id },
+    }));
+  }
   return (
     <div className={styles.normal}>
       {
-        list && list.map(i => <div className={styles.item} key={i.id}>
+        list && list.map(i => <div onClick={gotoUrl.bind(this, i.id)} className={styles.item} key={i.id}>
           <img className={styles.cover} src={i.cover} alt="" />
+          <span className={styles.title}>{i.title}</span>
         </div>)
       }
     </div>
--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { connect } from 'dva';
 import { routerRedux } from 'dva/router';
 import AppBar from 'material-ui/AppBar';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
 import IconButton from 'material-ui/IconButton';
 import ActionSearch from 'material-ui/svg-icons/action/search';
 import styles from './IndexPage.less';
 import BookShelf from '../components/BookShelf/BookShelf';
+
 
 function IndexPage({ dispatch, list }) {
   function gotoUrl() {
@@ -24,7 +27,13 @@ function IndexPage({ dispatch, list }) {
         }
         titleStyle={{ textAlign: 'center' }}
       />
-      <BookShelf list={list} />
+      <BookShelf dispatch={dispatch} list={list} />
+      <div className={styles.add}>
+        <FloatingActionButton iconStyle={{ width: 70, height: 70 }} style={{ width: 70, height: 70 }} onClick={gotoUrl}>
+          <ContentAdd style={{ width: 40, height: 40, marginTop: 3 }} />
+        </FloatingActionButton>
+      </div>
+
     </div>
   );
 }
