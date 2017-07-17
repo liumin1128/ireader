6e53e32adf7455e54ebb68cf25c641d4ce78d4a3
liumin1128 <970568830@qq.com>
2017-07-17 09:15:28 +0800

    新增reader的service方法

---
+++ src/services/reader.js
@@ -0,0 +1,16 @@
+import request from '../utils/request';
+
+// 获取书源
+export function getSource({ id }) {
+  return request(`/api/toc?view=summary&book=${id}`);
+}
+
+// 获取章节列表
+export function getChapterList({ id }) {
+  return request(`/api/toc/${id}?view=chapters`);
+}
+
+// 获取章节内容
+export function getChapter({ link }) {
+  return request(`/chapter/${link}?k=2124b73d7e2e1945&t=1468223717`);
+}
