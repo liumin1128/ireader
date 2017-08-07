7618a2fb489e7493aa02f25239d2d10a802d0f71
liumin1128 <970568830@qq.com>
2017-08-07 22:38:17 +0800

    更新样式

--- src/components/FloatButton/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import styles from './index.less';
-import Plus from './plus.svg';
-
-export default () => (<div className={styles.button}>
-  <img className={styles.icon} src={Plus} alt="" />
-</div>);
--- src/routes/Reader/Head.js
+++ src/routes/Reader/Head.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
 import Headroom from 'react-headroom';
-import Ripples from 'react-ripples';
 import styles from './Head.less';
 import CloseIcon from './close.svg';
 
 class Header extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      theme: 'black',
-    };
     this.back = () => {
       this.props.history.push('/');
       window.scrollTo(0, 0);
@@ -17,25 +13,12 @@ class Header extends Component {
   }
   render() {
     return (
-      <Headroom
-        onUnpin={() => {
-          this.setState({
-            theme: 'white',
-          });
-        }}
-        onUnfix={() => {
-          this.setState({
-            theme: 'black',
-          });
-        }}
-      >
+      <Headroom >
         <div className={styles.head}>
           <span />
-          <Ripples>
-            <span className={styles.button} onClick={this.back}>
-              <img style={{ opacity: 0.5 }} src={CloseIcon} alt="" />
-            </span>
-          </Ripples>
+          <span className={styles.button} onClick={this.back}>
+            <img style={{ opacity: 0.5 }} src={CloseIcon} alt="" />
+          </span>
         </div>
       </Headroom>
     );
